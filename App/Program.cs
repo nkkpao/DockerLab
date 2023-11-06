0a48b02d455a2c99506fa0938e41d4b4bd4e94b1
nkkpao <efphooo@gmail.com>
2023-11-06 16:35:03 +0700

    Добавьте файлы проекта.

---
+++ App/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine();
+args = Environment.GetCommandLineArgs();
+
+if(args.Length <= 1)
+{
+    Console.WriteLine("Zero args");
+    while(true) { }
+}
+
+string filePath = args[1];
+
+bool isFileExist = File.Exists(filePath);
+if (!isFileExist)
+{
+    Console.WriteLine("File does not exist.");
+    while (true) { }
+}
+using (StreamReader reader = File.OpenText(filePath))
+{
+    string text = reader.ReadToEnd();
+    if (text == "") { Console.WriteLine("File is empty"); while (true) { } }
+    Console.WriteLine(text);
+    while (true) { }
+}
